2deac69f450b535a76e6c44ba5eb4c0dd9e8b93a
Theo Weller <weller.theodor@gmail.com>
2019-06-03 13:39:19 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,6 +1,21 @@
 export default function createStore(reducer) {
-  // add your code here
-}
+  let state = 0;
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    render()
+  }
+
+  const getState = () => {
+    return state
+  }
+
+  return {
+    dispatch,
+    getState
+  };
+
+};//end createStore
 
 function render() {
   const container = document.getElementById('container');
