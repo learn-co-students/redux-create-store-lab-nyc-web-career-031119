01e47858980edaa0d7c84140dafa67250e2e721d
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-06-03 13:40:14 -0400

    Done.

--- src/reducers/candyReducer.js
+++ src/reducers/candyReducer.js
@@ -1,8 +1,10 @@
+
 function candyReducer(state = [], action) {
   switch (action.type) {
     case 'ADD_CANDY':
       return [...state, action.candy];
     default:
+    console.log("Candy state!", state)
       return state;
   }
 }
